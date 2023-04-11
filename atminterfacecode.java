33a21f4552709d73b0c9cd5efa8a48c8b76e8a47
yashdhandar07 <130383463+yashdhandar07@users.noreply.github.com>
2023-04-11 18:00:07 +0530

    Create atminterfacecode.java

---
+++ atminterfacecode.java
@@ -0,0 +1,73 @@
+package com.company;
+import java.io.*;
+import java.util.Scanner;
+public class atminterfacecode {
+        //main method starts
+        public static void main(String args[] )
+        {
+            //declare and initialize balance, withdraw, and deposit
+            int balance = 100000, withdraw, deposit;
+
+            //create scanner class object to get choice of user
+            Scanner sc = new Scanner(System.in);
+
+            while(true)
+            {
+                System.out.println("Automated Teller Machine");
+                System.out.println("press 1 for Withdraw");
+                System.out.println("press 2 for Deposit");
+                System.out.println("press 3 for Check Balance");
+                System.out.println("press 4 for EXIT");
+                System.out.print("select the operation you want to perform:");
+
+                //get choice from user
+                int choice = sc.nextInt();
+                switch(choice)
+                {
+                    case 1:
+                        System.out.print("Enter money to be withdrawn:");
+
+                        //get the withdrawl money from user
+                        withdraw = sc.nextInt();
+
+                        //check whether the balance is greater than or equal to the withdrawal amount
+                        if(balance >= withdraw)
+                        {
+                            //remove the withdrawl amount from the total balance
+                            balance = balance - withdraw;
+                            System.out.println("Please collect your money");
+                        }
+                        else
+                        {
+                            //show custom error message
+                            System.out.println("Insufficient Balance");
+                        }
+                        System.out.println("");
+                        break;
+
+                    case 2:
+
+                        System.out.print("Enter money to be deposited:");
+
+                        //get deposite amount from te user
+                        deposit = sc.nextInt();
+
+                        //add the deposit amount to the total balanace
+                        balance = balance + deposit;
+                        System.out.println("Your Money has been successfully depsited");
+                        System.out.println("");
+                        break;
+
+                    case 3:
+                        //displaying the total balance of the user
+                        System.out.println("Balance : "+balance);
+                        System.out.println("");
+                        break;
+
+                    case 4:
+                        //exit from the menu
+                        System.exit(0);
+                }
+            }
+        }
+    }
